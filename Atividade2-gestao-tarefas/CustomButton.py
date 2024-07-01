b644ee6ff65c5916628a27d3a7be2c1c33978a73
unknown <lucas.cordeiro3030@gmail.com>
2024-07-01 11:11:48 -0300

    Ajustando botões, comecando a desenvolver o funcionamento

--- Atividade2-gestao-tarefas/CustomButton.py
+++ Atividade2-gestao-tarefas/CustomButton.py
@@ -1,10 +1,20 @@
 import flet as ft
 class CustomButton(ft.ElevatedButton):
-    def __init__(self, text, bgcolor, on_click=None):
+    def __init__(self, text, bgcolor, color, on_click=None, icon=None):
 
-        super.__init__(
+        super().__init__(
             text=text,
             bgcolor=bgcolor,
-            on_click = on_click
+            color=color,
+            on_click=on_click,
+            icon = icon,
         )
+    
+    def addTask(self):
+        print("Add Task")
 
+    def clearList(self):
+        print("Clear List")
+
+    def toogleVisibility(self):
+        print("Toogle Visibility")
--- Atividade2-gestao-tarefas/main.py
+++ Atividade2-gestao-tarefas/main.py
@@ -1,7 +1,51 @@
 import flet as ft
+from CustomButton import CustomButton
 
 def main(page: ft.Page):
-    btn =ft.ElevatedButton("Click Me")
-    page.add(btn)
+    
+    task = ft.TextField(label="Task")
+    listTask = []
+    
+    # listTask = ft.List(items=task.value)
+
+    def clickBtnAddTask(e):
+        btnAddTask.addTask()
+        listTask.append(task.value)
+        print(listTask) 
+        page.update()
+    
+    def clickBtnToogleVisibility(e):
+        btnAddTask.toogleVisibility()
+        page.update()
+    
+    def clickBtnClearList(e):
+        btnAddTask.clearList()
+        page.update()
+
+    btnAddTask = CustomButton(
+        text = "Add",
+        bgcolor = ft.colors.BLUE,
+        color = ft.colors.WHITE,
+        on_click=clickBtnAddTask,
+        icon=None
+    )
+    
+    btnToogleVisibility = CustomButton(
+        text = "Visibility",
+        bgcolor = ft.colors.GREEN,
+        color = ft.colors.WHITE,
+        on_click=clickBtnToogleVisibility,
+        icon=None
+    )
+
+    btnClearList = CustomButton(
+        text = "Clear",
+        bgcolor = ft.colors.RED,
+        color = ft.colors.WHITE,
+        on_click=clickBtnClearList,
+        icon=None
+    )
+
+    page.add(task, btnAddTask, btnClearList, btnToogleVisibility)
 
 ft.app(target=main)
